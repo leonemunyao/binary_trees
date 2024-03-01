a97f1a8c2bd7e637bd9eb039fdcbf846962fcf6c
leonemunyao <leonemunyao@gmail.com>
2024-03-02 01:56:59 +0300

    Task 0, changed the betty style and memory allocation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 /* Allocate memory for the new node */
-binary_tree_t *new_node;
-
-new_node = malloc(sizeof(binary_tree_t));
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 if (new_node == NULL)
 {
